c844717644642c86ee6f7c8dc29ca91aeb071eef
Fassou Mathias Kolie <151475303+Kolie11@users.noreply.github.com>
2025-07-08 01:35:45 +0200

    adding some changes in membership options

--- src/Membership.jsx
+++ src/Membership.jsx
@@ -1,42 +1,61 @@
-import React from 'react'
+import React from "react";
 
 function Membership() {
   return (
-    <div className='bg-gray-100 h-full'>
-        <h1 className='font-bold text-center text-6xl pt-20 pb-10'>Membership options</h1>
+    <div className="bg-gray-100 h-full">
+      <h1 className="font-bold text-center text-6xl pt-20 pb-10 text-gray-800">
+        Membership options
+      </h1>
 
-        <div className='box flex items-center justify-center gap-10'>
-
-<div>
-    <h2>Starter</h2>
-<span>$19/month</span>
-<hr />
-<p>☑️ 1 book/month</p>
-<p>☑️ Online forums</p>
-<a href="#">Subscribe now</a>
-</div>
-
-<div>
-    <h2>Starter</h2>
-<span>$19/month</span>
-<hr />
-<p>☑️ 1 book/month</p>
-<p>☑️ Online forums</p>
-<a href="#">Subscribe now</a>
-</div>
-
-<div>
-    <h2>Starter</h2>
-<span>$19/month</span>
-<hr />
-<p>☑️ 1 book/month</p>
-<p>☑️ Online forums</p>
-<a href="#">Subscribe now</a>
-</div>
-
+      <div className="box flex items-center justify-center gap-6">
+        <div className="border-2 border-gray-200 p-4 rounded-lg">
+          <h2 className="font-bold text-2xl text-gray-800 pb-4">Starter</h2>
+          <span className="text-xl">
+            <strong>$19 </strong>/month
+          </span>
+          <hr className="mt-4 border-gray-400" />
+          <p className="text-lg mt-3">☑️ 1 book/month</p>
+          <p className="text-lg mb-5">☑️ Online forums</p>
+          <a
+            href="#"
+            className="uppercase font-bold border-2 border-gray-800 p-3 rounded-lg inline-block text-center mt-2  text-gray-100 bg-gray-800 hover:bg-gray-500 hover:border-0 transition duration-500 ease"
+          >
+            Subscribe now
+          </a>
         </div>
+
+        <div className="border-2 border-gray-200 p-4 rounded-lg w-80 h-75 bg-blue-300 shadow-lg">
+          <h2 className="font-bold text-2xl text-gray-800 pb-7">Pro</h2>
+          <span className="text-xl">
+            <strong>$29 </strong>/month
+          </span>
+          <hr className="mt-4 border-gray-400" />
+          <p className="text-lg mt-3">☑️ 2 books/month</p>
+          <p className="text-lg mb-5">☑️ Virtual meetups</p>
+          <a
+            href="#"
+            className="text-center w-full uppercase font-bold border-2 border-gray-900 p-3 rounded-lg inline-block  bg-gray-100 mt-2 hover:bg-blue-100 transition duration-500 ease"
+          >
+            Subscribe now
+          </a>
+        </div>
+
+        <div className="border-2 border-gray-200 p-4 rounded-lg">
+          <h2 className="font-bold text-2xl text-gray-800 pb-4">Enterprise</h2>
+          <span className="text-xl font-bold">Custom</span>
+          <hr className="mt-4 border-gray-400" />
+          <p className="text-lg mt-3">☑️ Team access</p>
+          <p className="text-lg mb-5">☑️ Private sessions</p>
+          <a
+            href="#"
+            className="uppercase font-bold border-2 border-gray-800 p-3 rounded-lg inline-block mt-2 w-full text-center text-gray-100 bg-gray-800 hover:bg-gray-500 hover:border-0 transition duration-500 ease"
+          >
+            Talk to us
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Membership
+export default Membership;
